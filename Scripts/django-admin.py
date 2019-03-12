9067f2bd9f84c6639ff04882f2521ba4aa20a6be
Chronos4 <georgep7@outlook.com>
2019-03-13 00:08:07 +0200

    Download django

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\georg\onedrive\υπολογιστής\travel-agent\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
